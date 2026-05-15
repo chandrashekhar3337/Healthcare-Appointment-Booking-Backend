a0a4abad2676132497b1101a2bf428ddf36295fe
chandrashekhar3337 <shekharara926290@gmail.com>
2026-05-15 22:14:09 +0530

    changed route

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,18 @@
 import js from "@eslint/js";
 import globals from "globals";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
-]);
+export default [
+  js.configs.recommended,
+
+  {
+    languageOptions: {
+      globals: globals.node,
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+
+    rules: {
+      "no-unused-vars": "warn",
+    },
+  },
+];
